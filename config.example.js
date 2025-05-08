77d1a8137a73e867f3ff17c5e7d0dc6386b64adb
Atakan Karaçalı <atakan.karacali97@gmail.com>
2025-05-08 16:17:58 +0300

    Add .gitignore

--- config.example.js
+++
@@ -1,2 +0,0 @@
-// Rename this file to config.js and replace the URL with the actual API.
-const API_URL = "https://your-api-url.com/api/v1/articles";
---
+++ config.js
@@ -0,0 +1,1 @@
+const API_URL = "https://kal3ogas3w54lyk34kedabeqyq0cjjyz.lambda-url.us-east-1.on.aws/api/v1/articles";
